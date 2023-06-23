55f3d579f3c73e8ad66152ff6d7232c7a203ef53
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-23 15:37:29 +0800

    Update config files.

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
+import schemas from './sanity/schemas';
 
 const config = defineConfig({
   projectId: 'ix5qjxyu',
   dataset: 'production',
   title: 'Dothack CMS',
-  apiVersion: '21-06-2023',
+  apiVersion: '2023-06-21',
   basePath: '/admin',
   plugins: [deskTool()],
+  schema: { types: schemas },
 });
 
 export default config;
---
+++ sanity/sanity.utils.ts
@@ -0,0 +1,21 @@
+import { createClient, groq } from 'next-sanity';
+
+export async function getProjects() {
+  const client = createClient({
+    projectId: 'ix5qjxyu',
+    dataset: 'production',
+    apiVersion: '2023-06-21',
+  });
+
+  return client.fetch(
+    groq`*[_type == 'project']{
+        _id,
+        _createdAt,
+        name,
+        "slug": slug.current,
+        "image": image.asset->url,
+        url,
+        content
+    }`
+  );
+}
