b6224bff4db2f5c65d22a33fa87de3c1a1539acd
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-23 23:57:55 +0800

    Fix tailwind issue. Need to add as dev dependency. Add styling and content for project page.

---
+++ sanity/client-config.ts
@@ -0,0 +1,7 @@
+const clientConfig = {
+  projectId: 'ix5qjxyu',
+  dataset: 'production',
+  apiVersion: '2023-06-21',
+};
+
+export default clientConfig;
--- sanity/sanity.utils.ts
+++ sanity/sanity.utils.ts
@@ -1,14 +1,9 @@
 import { Project } from '@/types/Project';
 import { createClient, groq } from 'next-sanity';
+import clientConfig from './client-config';
 
 export async function getProjects(): Promise<Project[]> {
-  const client = createClient({
-    projectId: 'ix5qjxyu',
-    dataset: 'production',
-    apiVersion: '2023-06-21',
-  });
-
-  return client.fetch(
+  return createClient(clientConfig).fetch(
     groq`*[_type == 'project']{
         _id,
         _createdAt,
@@ -18,5 +13,20 @@ export async function getProjects(): Promise<Project[]> {
         url,
         content
     }`
+  );
+}
+
+export async function getProject(slug: string): Promise<Project> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`,
+    { slug }
   );
 }
